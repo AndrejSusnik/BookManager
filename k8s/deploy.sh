fe0420c13988f5ff01cb23172570e58fb2492f07
Andrej Susnik <susnik.andrej7@gmail.com>
2024-01-16 18:11:54 +0100

    Update deployment script

--- k8s/deploy.sh
+++ k8s/deploy.sh
@@ -1,5 +1,6 @@
-kubectl apply -f ingres-config.yaml --namespace ingress-nginx
 kubectl apply -f etcd.yaml --namespace ingress-nginx
 kubectl apply -f book-info-deployment.yaml --namespace ingress-nginx
 kubectl apply -f book-manager-deployment.yaml --namespace ingress-nginx
 kubectl apply -f login-register-deployment.yaml --namespace ingress-nginx
+kubectl apply -f openapi-deployment.yaml --namespace ingress-nginx
+kubectl apply -f ingres-config.yaml --namespace ingress-nginx
