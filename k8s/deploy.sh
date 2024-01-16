a9a968e40283656907961f0262d36056f0882069
Andrej Susnik <susnik.andrej7@gmail.com>
2024-01-16 23:01:34 +0100

    Update ip

--- k8s/deploy.sh
+++ k8s/deploy.sh
@@ -1,6 +1,16 @@
-kubectl apply -f etcd.yaml --namespace ingress-nginx
-kubectl apply -f book-info-deployment.yaml --namespace ingress-nginx
-kubectl apply -f book-manager-deployment.yaml --namespace ingress-nginx
-kubectl apply -f login-register-deployment.yaml --namespace ingress-nginx
-kubectl apply -f openapi-deployment.yaml --namespace ingress-nginx
-kubectl apply -f ingres-config.yaml --namespace ingress-nginx
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.9.5/deploy/static/provider/cloud/deploy.yaml
+kubectl apply -f ingres-config.yaml --namespace app-routing-system
+kubectl apply -f etcd.yaml --namespace app-routing-system
+kubectl apply -f book-info-deployment.yaml --namespace app-routing-system
+kubectl apply -f book-manager-deployment.yaml --namespace app-routing-system
+kubectl apply -f login-register-deployment.yaml --namespace app-routing-system
+kubectl apply -f openapi-deployment.yaml --namespace app-routing-system
+
+# kubectl apply -f ingres-config.yaml --namespace ingress-nginx
+# kubectl apply -f etcd.yaml --namespace ingress-nginx
+# kubectl apply -f book-info-deployment.yaml --namespace ingress-nginx
+# kubectl apply -f book-manager-deployment.yaml --namespace ingress-nginx
+# kubectl apply -f login-register-deployment.yaml --namespace ingress-nginx
+# kubectl apply -f openapi-deployment.yaml --namespace ingress-nginx
+
+
