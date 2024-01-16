62881efe275f1ab688b388db5c0197d7e6a421a4
Andrej Susnik <susnik.andrej7@gmail.com>
2024-01-16 17:28:12 +0100

    Update openapi

--- src/LoginRegisterService/LoginRegisterService.py
+++ src/LoginRegisterService/LoginRegisterService.py
@@ -10,7 +10,8 @@ app = Flask(__name__)
 app.config['API_TITLE'] = 'LoginRegisterService'
 app.config['API_VERSION'] = 'v1'
 app.config['OPENAPI_VERSION'] = '3.0.3'
-app.config['OPENAPI_URL_PREFIX'] = '/openapi/'
+app.config['OPENAPI_URL_PREFIX'] = '/'
+app.config['OPENAPI_JSON_PATH'] = 'login-register/openapi.json'
 app.config['OPENAPI_SWAGGER_UI_PATH'] = '/swagger-ui'
 app.config["OPENAPI_SWAGGER_UI_URL"] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
 
