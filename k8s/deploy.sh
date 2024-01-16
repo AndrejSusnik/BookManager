013b36e42cf32a2ba1d6c9ef519d7a90ad87aae4
Andrej Susnik <susnik.andrej7@gmail.com>
2024-01-16 17:15:02 +0100

    Fix openapi

--- k8s/deploy.sh
+++ k8s/deploy.sh
@@ -1,5 +1,5 @@
 kubectl apply -f ingres-config.yaml --namespace ingress-nginx
+kubectl apply -f etcd.yaml --namespace ingress-nginx
 kubectl apply -f book-info-deployment.yaml --namespace ingress-nginx
 kubectl apply -f book-manager-deployment.yaml --namespace ingress-nginx
 kubectl apply -f login-register-deployment.yaml --namespace ingress-nginx
-kubectl apply -f etcd.yaml --namespace ingress-nginx
